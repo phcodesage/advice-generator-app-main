e626d6ca5e05cf9c022545fe5758e1478f34c239
Rechcel Toledo <rechceltoledo@gmail.com>
2022-10-23 11:31:19 +0800

    added app.js

---
+++ app/app.js
@@ -0,0 +1,36 @@
+ document.addEventListener("DOMContentLoaded", () => {
+    
+ //DOM ELEMENTS
+ const btn = document.querySelector(".advice-btn");
+ const quote = document.getElementById("#advice-description");
+ const titleID = document.querySelector(".advice-title");
+
+
+
+ async function updateQuote() {
+     //Fetch a random quote from the https://api.adviceslip.com
+        
+     const response = await fetch('https://api.advicesslip.com/advice');
+     const data = await response.json();
+     const slip = data.slip;
+     if (response.ok) {
+         // Update DOM elements
+         quote.innerHTML= `<q>${slip.advice}</q>`;
+         titleID.textContent = 'ADVICE #: '+ slip.id;
+         console.log(slip.advice);
+     } else {
+         quote.textContent = "An error occured"
+        console.log(data);
+        }
+        
+ }
+ //attach an event listener to the 'button'
+    btn.addEventListener("click", updateQuote);
+
+ // call getAdvice once when page loads
+
+updateQuote();
+ })
+
+
+
