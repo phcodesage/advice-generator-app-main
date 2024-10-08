d6db1f6a0e6f1e051ebbced0d964344825aa6a18
phcodesage <rechceltoledo@gmail.com>
2024-10-09 01:03:49 +0800

    fixed: responsiveness

--- app/app.js
+++ app/app.js
@@ -1,36 +1,41 @@
- document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     
- //DOM ELEMENTS
- const btn = document.querySelector(".advice-btn");
- const quote = document.getElementById("advice-description");
- const titleID = document.querySelector(".advice-title");
+    //DOM ELEMENTS
+    const btn = document.querySelector(".advice-btn");
+    const quote = document.getElementById("advice-description");
+    const titleID = document.querySelector(".advice-title");
 
+    async function updateQuote() {
+        // Add animation class
+        btn.classList.add('animate');
+        quote.classList.add('loading');
 
-
- async function updateQuote() {
-     //Fetch a random quote from the https://api.adviceslip.com
+        //Fetch a random quote from the https://api.adviceslip.com
+        const response = await fetch('https://api.adviceslip.com/advice');
+        const data = await response.json();
+        const slip = data.slip;
         
-     const response = await fetch('https://api.adviceslip.com/advice');
-     const data = await response.json();
-     const slip = data.slip;
-     if (response.ok) {
-         // Update DOM elements
-         quote.innerHTML= `<q>${slip.advice}</q>`;
-         titleID.textContent = 'ADVICE #: '+ slip.id;
-         console.log(slip.advice);
-     } else {
-         quote.textContent = "An error occured"
-        console.log(data);
+        if (response.ok) {
+            // Update DOM elements
+            quote.innerHTML = `<q>${slip.advice}</q>`;
+            titleID.textContent = 'ADVICE #: ' + slip.id;
+        } else {
+            quote.textContent = "An error occurred";
+            console.log(data);
         }
         
- }
- //attach an event listener to the 'button'
+        // Remove animation class after 2 seconds
+
+        setTimeout(() => {
+            btn.classList.remove('animate');
+            quote.classList.remove('loading');
+        }, 2000);
+    }
+
+    //attach an event listener to the 'button'
     btn.addEventListener("click", updateQuote);
 
- // call getAdvice once when page loads
-
-updateQuote();
- })
-
-
+    // call updateQuote once when page loads
+    updateQuote();
+})
 
